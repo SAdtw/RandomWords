62934e34a81c7b0283b93d9043747e2fef497239
SAkagi <ichiwri@gmail.com>
2018-12-26 00:46:43 +0900

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="random_words",
+    version="0.1.0",
+    author="SA",
+    author_email="",
+    description="create_randowm_words",
+    long_description=long_description
+    )
+
